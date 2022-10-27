242d5a438a43db6e1ba07ff117fd1f84045d45cc
Luis Gustavo <luisgustavo3010@hotmail.com>
2022-10-27 16:49:37 -0300

    semana 4 - crud cadastro vaga

--- estacionamento/src/connection/script.sql
+++ estacionamento/src/connection/script.sql
@@ -2,9 +2,21 @@ CREAT DATABASE IF NOT EXIST estacionamento;
 USE estacionemento;
 
 CREATE TABLE vaga (
-    idVage int NOT NULL AUTO_INCREMENT,
-    numero int NOT NULL,
-    rua varchar(100) NOT NULL,
-    obliqua boolean NOT NULL,
-    PRIMARY KEY (idVaga));
-)
+    idVaga      int NOT NULL AUTO_INCREMENT,
+    numero      int NOT NULL,
+    rua         varchar(100) NOT NULL,
+    obliqua     boolean NOT NULL,
+    PRIMARY KEY (idVaga)
+);
+
+create table motorista (
+    idMotorista int NOT NULL AUTO_INCREMENT,
+    nome        varchar (100) NOT NULL,
+    genero      varchar(32) NOT NULL,
+    rg          int NOT NULL,
+    cpf         int NOT NULL,
+    celular     int NOT NULL,
+    email       varchar (50),
+    senha       varchar (32)
+    PRIMARY KEY (idMotorista)
+);
---
+++ estacionamento/src/model/bean/Motorista.java
@@ -0,0 +1,78 @@
+package model.bean;
+
+public class Motorista {
+    private int idMotorista;
+    private String nome;
+    private String genero;
+    private int rg;
+    private int cpf;
+    private int numero;
+    private String email;
+    private String senha;
+
+    public int getIdMotorista() {
+        return idMotorista;
+    }
+
+    public void setIdMotorista(int idMotorista) {
+        this.idMotorista = idMotorista;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
+
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getRg() {
+        return rg;
+    }
+
+    public void setRg(int rg) {
+        this.rg = rg;
+    }
+
+    public int getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(int cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+    
+    
+}
