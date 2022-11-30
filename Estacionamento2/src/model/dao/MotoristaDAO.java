9413ec38ba82bea0a81e9fed79b070258032ca4c
Luis Gustavo <luisgustavo3010@hotmail.com>
2022-11-30 15:14:14 -0300

    Final version(GRAÇAS A DEUS)

--- Estacionamento2/src/model/dao/MotoristaDAO.java
+++ Estacionamento2/src/model/dao/MotoristaDAO.java
@@ -11,13 +11,13 @@ import javax.swing.JOptionPane;
 import model.bean.Motorista;
 
 public class MotoristaDAO {
-    
-        public void create(Motorista m) {
+
+    public void create(Motorista m) {
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
 
-        try{
-            stmt = con.prepareStatement ("INSERT INTO motorista (nome, genero, rg, cpf, celular, email, senha) VALUES (?, ?, ?, ?, ?, ?, ?)"); 
+        try {
+            stmt = con.prepareStatement("INSERT INTO motorista (nome, genero, rg, cpf, celular, email, senha) VALUES (?,?,?,?,?,?,?)");
             stmt.setString(1, m.getNome());
             stmt.setString(2, m.getGenero());
             stmt.setInt(3, m.getRg());
@@ -28,67 +28,66 @@ public class MotoristaDAO {
             stmt.executeUpdate();
 
             JOptionPane.showMessageDialog(null, "Motorista cadastrado com sucesso!");
-        }catch(SQLException e){
-             JOptionPane.showMessageDialog(null, "Erro ao cadastrar motorista: "+ e);
-        }finally {
+        } catch (SQLException e) {
+            JOptionPane.showMessageDialog(null, "Erro ao cadastrar motorista: " + e);
+        } finally {
             ConnectionFactory.closeConnection(con, stmt);
-        } 
+        }
     }
-    public List<Motorista> read(){
+
+    public List<Motorista> read() {
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
         ResultSet rs = null;
         List<Motorista> motoristas = new ArrayList<>();
-        try{
+        try {
             stmt = con.prepareStatement("SELECT * FROM motorista;");
             rs = stmt.executeQuery();
-            while(rs.next()){
+            while (rs.next()) {
                 Motorista m = new Motorista();
                 m.setIdMotorista(rs.getInt("idMotorista"));
                 m.setNome(rs.getString("nome"));
                 m.setGenero(rs.getString("genero"));
                 m.setRg(rs.getInt("rg"));
-                m.setCpf(rs.getInt("cpg"));
+                m.setCpf(rs.getInt("cpf"));
                 m.setNumero(rs.getInt("celular"));
                 m.setEmail(rs.getString("email"));
                 m.setSenha(rs.getString("senha"));
                 motoristas.add(m);
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException("Erro ao buscar os dados: ", e);
-        }finally{
+        } finally {
             ConnectionFactory.closeConnection(con, stmt, rs);
         }
         return motoristas;
     }
-    
-    public void delete(Motorista m){
+
+    public void delete(Motorista m) {
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
-        
-        try{
-            stmt = con.prepareStatement("DELETE FROM motorista WHERE idMotorista=?");
-            stmt.setInt(1,m.getIdMotorista());
+        try {
+            stmt = con.prepareStatement("DELETE  FROM motorista where idMotorista =?");
+            stmt.setInt(1, m.getIdMotorista());
             stmt.executeUpdate();
-            
-            JOptionPane.showMessageDialog(null, "Motorista excluído com sucesso!");
-        }catch(SQLException e){
-            JOptionPane.showMessageDialog(null, "Erro ao excluir: " + e);
-        }finally{
+            JOptionPane.showMessageDialog(null, "Motorista excluído com sucesso");
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao deletar motorista: " + e);
+        } finally {
             ConnectionFactory.closeConnection(con, stmt);
         }
     }
-    
-    public Motorista read(int idMotorista){
+
+    public Motorista read(int idMotorista) {
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
         ResultSet rs = null;
         Motorista m = new Motorista();
-        try{
+        try {
             stmt = con.prepareStatement("SELECT * FROM motorista WHERE idMotorista=? LIMIT 1;");
             stmt.setInt(1, idMotorista);
             rs = stmt.executeQuery();
-            if(rs != null && rs.next()){
+            if (rs != null && rs.next()) {
                 m.setIdMotorista(rs.getInt("idMotorista"));
                 m.setNome(rs.getString("nome"));
                 m.setGenero(rs.getString("genero"));
@@ -98,33 +97,34 @@ public class MotoristaDAO {
                 m.setEmail(rs.getString("email"));
                 m.setSenha(rs.getString("senha"));
             }
-        }catch (SQLException e){
+        } catch (SQLException e) {
             throw new RuntimeException("Erro ao buscar os dados", e);
-        }finally{
+        } finally {
             ConnectionFactory.closeConnection(con, stmt, rs);
         }
         return m;
     }
-    
-    public void update(Motorista m){
+
+    public void update(Motorista m) {
         Connection con = ConnectionFactory.getConnection();
         PreparedStatement stmt = null;
-        try{
-            stmt = con.prepareStatement ("UPDATE motorista SET nome=?, genero=?, rg=?, cpf=?, celular=?, email=?, senha=?"); 
-            stmt.setString(1, m.getNome());
+        try {
+            stmt = con.prepareStatement("UPDATE motorista SET nome=?, genero=?, rg=?, cpf=?, celular=?, email=?, senha=? WHERE idMotorista=?");            stmt.setString(1, m.getNome());
             stmt.setString(2, m.getGenero());
             stmt.setInt(3, m.getRg());
             stmt.setInt(4, m.getCpf());
             stmt.setInt(5, m.getNumero());
             stmt.setString(6, m.getEmail());
             stmt.setString(7, m.getSenha());
+
             stmt.executeUpdate();
             JOptionPane.showMessageDialog(null, "Motorista atualizado com sucesso!");
-        }catch (SQLException e){
+            System.out.println(stmt);
+        } catch (SQLException e) {
             JOptionPane.showMessageDialog(null, "Erro ao atualizar: " + e);
-        }finally{
+        } finally {
             ConnectionFactory.closeConnection(con, stmt);
         }
     }
-    
+
 }
--- Estacionamento2/src/view/JFListarMotoristas.java
+++ Estacionamento2/src/view/JFListarMotoristas.java
@@ -1,9 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package view;
 
 import javax.swing.JOptionPane;
-import javax.swing.table.DefaultTableModel;
 import model.dao.MotoristaDAO;
 import model.bean.Motorista;
+import javax.swing.table.DefaultTableModel;
 
 public class JFListarMotoristas extends javax.swing.JFrame {
 
@@ -24,57 +29,55 @@ public class JFListarMotoristas extends javax.swing.JFrame {
     private void initComponents() {
 
         jLabel1 = new javax.swing.JLabel();
-        jBtnExcluir = new javax.swing.JButton();
-        jBtnCadastrar = new javax.swing.JButton();
-        jBtnEditar = new javax.swing.JButton();
-        JTMotorista1 = new javax.swing.JScrollPane();
+        jScrollPane1 = new javax.swing.JScrollPane();
         jTMotorista = new javax.swing.JTable();
+        jBtnCancelar = new javax.swing.JButton();
+        jBtnEditar = new javax.swing.JButton();
+        jBtnExcluir = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        jLabel1.setFont(new java.awt.Font("Tahoma", 1, 36)); // NOI18N
-        jLabel1.setText("Listar Motoristas");
-
-        jBtnExcluir.setText("Excluir Motorista");
-        jBtnExcluir.addActionListener(new java.awt.event.ActionListener() {
-            public void actionPerformed(java.awt.event.ActionEvent evt) {
-                jBtnExcluirActionPerformed(evt);
+        addWindowListener(new java.awt.event.WindowAdapter() {
+            public void windowOpened(java.awt.event.WindowEvent evt) {
+                formWindowOpened(evt);
             }
         });
 
-        jBtnCadastrar.setText("Cadastrar Motorista");
+        jLabel1.setFont(new java.awt.Font("Gadugi", 1, 36)); // NOI18N
+        jLabel1.setText("Listar Motoristas");
 
-        jBtnEditar.setText("Editar Motorista");
+        jTMotorista.setModel(new javax.swing.table.DefaultTableModel(
+            new Object [][] {
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null},
+                {null, null, null, null, null, null, null, null}
+            },
+            new String [] {
+                "ID Motorista", "Nome", "Genero", "RG", "CPF", "Celular", "Email", "Senha"
+            }
+        ));
+        jTMotorista.addVetoableChangeListener(new java.beans.VetoableChangeListener() {
+            public void vetoableChange(java.beans.PropertyChangeEvent evt)throws java.beans.PropertyVetoException {
+                jTMotoristaVetoableChange(evt);
+            }
+        });
+        jScrollPane1.setViewportView(jTMotorista);
+
+        jBtnCancelar.setText("Cancelar");
+
+        jBtnEditar.setText("Editar");
         jBtnEditar.addActionListener(new java.awt.event.ActionListener() {
             public void actionPerformed(java.awt.event.ActionEvent evt) {
                 jBtnEditarActionPerformed(evt);
             }
         });
 
-        jTMotorista.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null}
-            },
-            new String [] {
-                "Id motorista", "Nome", "Gênero", "RG", "CPF", "Celular", "Email"
-            }
-        ) {
-            boolean[] canEdit = new boolean [] {
-                false, false, false, false, false, false, false
-            };
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
+        jBtnExcluir.setText("Excluir");
+        jBtnExcluir.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                jBtnExcluirActionPerformed(evt);
             }
         });
-        JTMotorista1.setViewportView(jTMotorista);
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
         getContentPane().setLayout(layout);
@@ -83,15 +86,20 @@ public class JFListarMotoristas extends javax.swing.JFrame {
             .addGroup(layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(jLabel1)
                     .addGroup(layout.createSequentialGroup()
-                        .addComponent(jBtnCadastrar)
-                        .addGap(31, 31, 31)
-                        .addComponent(jBtnEditar)
-                        .addGap(28, 28, 28)
-                        .addComponent(jBtnExcluir))
-                    .addComponent(JTMotorista1, javax.swing.GroupLayout.PREFERRED_SIZE, 691, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(36, Short.MAX_VALUE))
+                        .addComponent(jLabel1)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(layout.createSequentialGroup()
+                        .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 714, Short.MAX_VALUE)
+                            .addGroup(layout.createSequentialGroup()
+                                .addComponent(jBtnCancelar)
+                                .addGap(18, 18, 18)
+                                .addComponent(jBtnEditar)
+                                .addGap(18, 18, 18)
+                                .addComponent(jBtnExcluir)
+                                .addGap(0, 0, Short.MAX_VALUE)))
+                        .addContainerGap())))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -99,69 +107,75 @@ public class JFListarMotoristas extends javax.swing.JFrame {
                 .addContainerGap()
                 .addComponent(jLabel1)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(JTMotorista1, javax.swing.GroupLayout.PREFERRED_SIZE, 319, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 38, Short.MAX_VALUE)
+                .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, 354, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
                 .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(jBtnCadastrar)
+                    .addComponent(jBtnCancelar)
                     .addComponent(jBtnEditar)
                     .addComponent(jBtnExcluir))
-                .addContainerGap())
+                .addContainerGap(62, Short.MAX_VALUE))
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
 
-    private void formWindowOpened(java.awt.event.WindowEvent evt) {
-        readJTable();
-    }
+    private void jTMotoristaVetoableChange(java.beans.PropertyChangeEvent evt)throws java.beans.PropertyVetoException {//GEN-FIRST:event_jTMotoristaVetoableChange
+        // TODO add your handling code here:
+    }//GEN-LAST:event_jTMotoristaVetoableChange
 
-    public void readJTable() {
-        DefaultTableModel modelo = (DefaultTableModel) jTMotorista.getModel();
-        modelo.setNumRows(0);
-        MotoristaDAO dao = new MotoristaDAO();
-        for (Motorista m : dao.read()) {
-            System.out.println(m.getNome());
-            modelo.addRow(new Object[]{
-                m.getIdMotorista(),
-                m.getNome(),
-                m.getGenero(),
-                m.getRg(),
-                m.getCpf(),
-                m.getNumero(),
-                m.getEmail(),});
-        }
-    }
+    private void formWindowOpened(java.awt.event.WindowEvent evt) {//GEN-FIRST:event_formWindowOpened
+        readJTable();
+    }//GEN-LAST:event_formWindowOpened
 
     private void jBtnExcluirActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnExcluirActionPerformed
         if (jTMotorista.getSelectedRow() != -1) {
             int opcao = JOptionPane.showConfirmDialog(null,
-                    "Deseja excluir o motorista selecionado?", "Exclusão",
-                    JOptionPane.YES_NO_OPTION);
-            if (opcao == 0) {
+                    "Deseja excluir a vaga selecionada?", "Exclusão", JOptionPane.YES_NO_OPTION);
+            if(opcao == 0) {
                 MotoristaDAO dao = new MotoristaDAO();
-                Motorista m = new Motorista();
-                m.setIdMotorista((int) jTMotorista.getValueAt(jTMotorista.getSelectedRow(), 0));
-                dao.delete(m);
+                Motorista v = new Motorista();
+                v.setIdMotorista((int)jTMotorista.getValueAt(jTMotorista.getSelectedRow(), 0));
+                dao.delete(v);
             }
         } else {
-            JOptionPane.showMessageDialog(null, "Selecione um motorista!", "Erro",
+            JOptionPane.showMessageDialog(null, "Selecione um motorista!", "Erro", 
                     JOptionPane.ERROR_MESSAGE);
         }
         readJTable();
+// TODO add your handling code here:
     }//GEN-LAST:event_jBtnExcluirActionPerformed
 
     private void jBtnEditarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jBtnEditarActionPerformed
-        if (jTMotorista.getSelectedRow() != -1) {
-            int motoristaSelecionado = (int) jTMotorista.getValueAt(jTMotorista.getSelectedRow(), 0);
+        // TODO add your handling code here:
+         if (jTMotorista.getSelectedRow() != -1) {
+            int motoristaSelecionado = (int)jTMotorista.getValueAt(jTMotorista.getSelectedRow(), 0);
             JFAtualizarMotorista am = new JFAtualizarMotorista(motoristaSelecionado);
             am.setVisible(true);
+            System.out.println(motoristaSelecionado);
+            
         } else {
-            JOptionPane.showMessageDialog(null, "Selecione um motorista!",
-                    "Erro", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showMessageDialog(null, "Selecione um motorista!", "ERRO", JOptionPane.ERROR_MESSAGE);
+            
         }
         readJTable();
     }//GEN-LAST:event_jBtnEditarActionPerformed
-
+    public void readJTable() {
+        DefaultTableModel modelo = (DefaultTableModel) jTMotorista.getModel();
+        modelo.setNumRows(0);
+        MotoristaDAO dao = new MotoristaDAO();
+        for (Motorista m: dao.read()) {
+            modelo.addRow(new Object[] {
+            m.getIdMotorista(),
+            m.getNome(),
+            m.getGenero(),
+            m.getRg(),
+            m.getCpf(),
+            m.getNumero(),
+            m.getEmail(),
+            m.getSenha()                    
+        });
+        }    
+    }
     /**
      * @param args the command line arguments
      */
@@ -188,6 +202,7 @@ public class JFListarMotoristas extends javax.swing.JFrame {
             java.util.logging.Logger.getLogger(JFListarMotoristas.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
         //</editor-fold>
+        //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
@@ -198,11 +213,11 @@ public class JFListarMotoristas extends javax.swing.JFrame {
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JScrollPane JTMotorista1;
-    private javax.swing.JButton jBtnCadastrar;
+    private javax.swing.JButton jBtnCancelar;
     private javax.swing.JButton jBtnEditar;
     private javax.swing.JButton jBtnExcluir;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JTable jTMotorista;
     // End of variables declaration//GEN-END:variables
 }
